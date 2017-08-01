1379c344188cb177f1dbe862c239c776e7e2f589
Sander <dsander17@gmail.com>
2017-07-31 19:23:00 -0700

    Milestone logging

--- src/main/java/com/iota/iri/Milestone.java
+++ src/main/java/com/iota/iri/Milestone.java
@@ -60,6 +60,7 @@ public class Milestone {
 
     private boolean shuttingDown;
     private static int RESCAN_INTERVAL = 5000;
+    private long lastMilestoneLog = 0;
     private long lastSolidMilestoneLog = 0;
 
     public void init(final SpongeFactory.Mode mode, final LedgerValidator ledgerValidator, final boolean revalidate) {
@@ -100,6 +101,15 @@ public class Milestone {
                         messageQ.publish("lmi %d %d", previousLatestMilestoneIndex, latestMilestoneIndex);
                         log.info("Latest milestone has changed from #" + previousLatestMilestoneIndex
                                 + " to #" + latestMilestoneIndex);
+                        lastMilestoneLog = System.currentTimeMillis();
+                    }
+                    else
+                    {
+                    	if(System.currentTimeMillis() - lastMilestoneLog >= 300000)
+                    	{
+                    		log.info("Latest milestone has NOT changed! #" + latestMilestoneIndex);
+                    		lastMilestoneLog = System.currentTimeMillis();
+                    	}
                     }
 
                     Thread.sleep(Math.max(1, RESCAN_INTERVAL - (System.currentTimeMillis() - scanTime)));
